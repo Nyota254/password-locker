a4204dddbb5db100a8024d052426275bd32f2e53
Nyota254 <knyota66@gmail.com>
2019-08-30 12:22:56 +0300

    add the test for initialisation of credentials objects

--- test-class-password-locker.py
+++ test-class-password-locker.py
@@ -23,7 +23,7 @@ class TestUsers(unittest.TestCase):
         # self.new_user = User("admin","admin")
         self.assertEqual(self.new_user.user_name,"admin")
         self.assertEqual(self.new_user.user_password,"admin")
-        
+
     def test_save_user(self):
         '''
         This is a test to see whether the user can save their user name
@@ -31,5 +31,20 @@ class TestUsers(unittest.TestCase):
         # self.new_user = User("admin","admin")
         self.new_user.save_user()
         self.assertEqual(len(User.user_details),1)
+
+###########################################################################################
+#CREDENTIALS TESTS BEGIN HERE
+###########################################################################################
+
+    def test_credentials_init(self):
+        '''
+        This test tests if the class credentials instanciates the data
+        '''
+        self.assertEqual(self.new_credentials.user_name,"admin")
+        self.assertEqual(self.new_credentials.site,"twitter")
+        self.assertEqual(self.new_credentials.account_name,"admin")
+        self.assertEqual(self.new_credentials.account_email,"admin@admin.com")
+        self.assertEqual(self.new_credentials.account_password,"123456")
+
 
         
