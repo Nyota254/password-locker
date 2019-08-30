ddce313b2615dd9c5b8824b820509773fe3bc717
Nyota254 <knyota66@gmail.com>
2019-08-30 12:19:33 +0300

    add the test for saving users

--- test-class-password-locker.py
+++ test-class-password-locker.py
@@ -23,6 +23,13 @@ class TestUsers(unittest.TestCase):
         # self.new_user = User("admin","admin")
         self.assertEqual(self.new_user.user_name,"admin")
         self.assertEqual(self.new_user.user_password,"admin")
-
+        
+    def test_save_user(self):
+        '''
+        This is a test to see whether the user can save their user name
+        '''
+        # self.new_user = User("admin","admin")
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_details),1)
 
         
