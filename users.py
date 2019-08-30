27397b3f335d26029d4935ba33a8f78653e32d47
Nyota254 <knyota66@gmail.com>
2019-08-30 12:17:37 +0300

    add the initialisation for the users class

--- users.py
+++ users.py
@@ -1,2 +1,16 @@
 
 
+class User:
+    '''
+    Class for storing user information such as name and password
+    '''
+    user_details = []
+    def __init__(self,user_name,user_password):
+        '''
+        Initialise method that helps us define properties for our object
+        Arg:
+            user_name: new Users user_name
+            user_password:new Users user_password
+        '''
+        self.user_name = user_name
+        self.user_password = user_password
